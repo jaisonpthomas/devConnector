a5ea5566dfcf038f773a1300eb252832697f424a
Jaison Thomas <email.jaisonthomas@gmail.com>
2019-05-05 17:44:38 -0500

    alert component and action

--- client/src/App.js
+++ client/src/App.js
@@ -6,6 +6,7 @@ import store from "./store";
 //Components
 import Navbar from "./components/layout/Navbar";
 import Landing from "./components/layout/Landing";
+import Alert from "./components/layout/Alert";
 import Register from "./components/auth/Register";
 import Login from "./components/auth/Login";
 
@@ -18,6 +19,7 @@ const App = () => (
         <Navbar />
         <Route exact path="/" component={Landing} />
         <section className="container">
+          <Alert />
           <Switch>
             <Route exact path="/register" component={Register} />
             <Route exact path="/login" component={Login} />
